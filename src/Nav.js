fd07098a8b2a06197169c6441f5fcceb9daf4f5e
ICR\jx.chuah <jx.chuah@felixdigital.biz>
2024-06-07 17:01:16 +0800

    semantic and meta tag

---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+function Nav() {
+    return (
+        <nav className="header-nav">
+            <ul>
+                <li><a href="home.html">HOME</a></li>
+                <li><a href="about.html">ABOUT</a></li>
+                <li><a href="menu.html">MENU</a></li>
+                <li><a href="reservations.html">RESERVATIONS</a></li>
+                <li><a href="order.html">ORDER ONLINE</a></li>
+                <li><a href="login.html">LOGIN</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
